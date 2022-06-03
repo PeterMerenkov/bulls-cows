0c445424ff51da86eb82efdede7c28e248e8c198
Пётр Меренков <peter.merenkov@gmail.com>
2022-06-03 14:50:58 +0400

    Initial commit

---
+++ src/main/java/ru/merenkov/bullscows/BullsCowsApplication.java
@@ -0,0 +1,13 @@
+package ru.merenkov.bullscows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullsCowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullsCowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/merenkov/bullscows/BullsCowsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.merenkov.bullscows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullsCowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
