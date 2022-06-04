d38faaf7b75a232463159566ef90cb80d238f854
Пётр Меренков <peter.merenkov@gmail.com>
2022-06-04 15:00:30 +0400

    fill service and create generator of unique sequences

--- src/main/java/ru/merenkov/bullscows/services/GameService.java
+++ src/main/java/ru/merenkov/bullscows/services/GameService.java
@@ -1,8 +1,36 @@
 package ru.merenkov.bullscows.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.merenkov.bullscows.repos.GameJdbcRepo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class GameService {
+    GameJdbcRepo gameJdbcRepo;
+
+    @Autowired
+    public GameService(GameJdbcRepo gameJdbcRepo) {
+        this.gameJdbcRepo = gameJdbcRepo;
+    }
+
+    public List<Integer> generateUniqueSeq() {
+        List<Integer> arr = new ArrayList<>(4);
+
+        Random random = new Random();
+        for (int i = 0; i < 4;) {
+            int randNum = random.nextInt(10);
+            random.nextInt(10);
+            if (!arr.contains(randNum)) {
+                arr.add(randNum);
+                i++;
+            }
+        }
+
+        return arr;
+    }
 
 }
