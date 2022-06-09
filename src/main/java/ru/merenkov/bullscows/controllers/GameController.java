de3415d5bc39a1e7b2484a5ea60e2f14677ff531
Пётр Меренков <peter.merenkov@gmail.com>
2022-06-09 12:47:24 +0400

    autowire GameService to GameController

--- src/main/java/ru/merenkov/bullscows/controllers/GameController.java
+++ src/main/java/ru/merenkov/bullscows/controllers/GameController.java
@@ -1,8 +1,15 @@
 package ru.merenkov.bullscows.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import ru.merenkov.bullscows.services.GameService;
 
 @Controller
 public class GameController {
+    GameService gameService;
 
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
 }
